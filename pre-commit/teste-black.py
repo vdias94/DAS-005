484ef0e49f1434cc02de85b746f71a0f78e4872f
Victor <victordias94@hotmail.com>
2022-09-23 00:48:08 -0300

    check commit revision

---
+++ pre-commit/teste-black.py
@@ -0,0 +1,17 @@
+"""Descrição do Módulo Teste"""
+import numpy as np
+
+
+def function_to_print(
+    long_variable_text_01=None,
+    long_variable_text_02=None,
+    long_variable_text_03=None,
+    long_variable_text_04=None,
+    long_variable_text_05=None,
+    long_variable_text_06=None,
+):
+    """Descrição Teste"""
+    print(f"Olá meu caro Watson, seu número é {np.random.randint(1, 6)}")
+
+
+function_to_print()
